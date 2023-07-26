4cdf25ca864c043c10fc07b751b22bd038316b74
qalbeabbas947 <coordinator947@gmail.com>
2023-07-26 16:25:13 +0500

    Added the light box for sharable image

--- includes/shortcodes/class-reviews.php
+++ includes/shortcodes/class-reviews.php
@@ -50,9 +50,9 @@ class ldFMT_Reviews_Shortcode {
             ?>
                 <div class="review-container">
                     <?php if(!empty($review->picture)) { ?>
-                        <a class="ldfmt_review_image" href="<?php echo $review->profile_url;?>"><img src="<?php echo $review->picture;?>" alt="Avatar" style="width:90px"></a>
+                        <a class="ldfmt_review_image-link" href="<?php echo $review->profile_url;?>"><img class="ldfmt_review_image" src="<?php echo $review->picture;?>" alt="Avatar" style="width:90px"></a>
                     <?php } ?>
-                    <h3 class="ldfmt_review_title"><a href="<?php echo $review->sharable_img;?>"><?php echo $review->title;?></a></h3>
+                    <h3 class="ldfmt_review_title"><a class="ldfmt_review_title-link" href="<?php echo $review->sharable_img;?>" data-lightbox="ldfmt-set" data-title="<?php echo $review->title;?>"><?php echo $review->title;?></a></h3>
                     <p class="ldfmt_review_user"><span><?php echo $review->name;?></span> of <?php echo !empty($review->company)?'<a href="'.$review->company_url.'">'.$review->company.'</a>':''; ?></p>
                     <p class="ldfmt_review_description"><?php echo $review->text;?></p>
                     <p class="ldfmt_review_time_wrapper"><div class="ldfmt_review_time"><?php echo $review->created;?></div><div class="ldfmt_review_rate"><?php echo __('Rate:', LDNFT_TEXT_DOMAIN);?> <?php echo $review->rate;?></div></p>
@@ -63,43 +63,6 @@ class ldFMT_Reviews_Shortcode {
             echo '<div class="ldfmt-no-results">'.__('No review(s) found.', LDNFT_TEXT_DOMAIN).'</div>';
         }
         exit;
-    }
-
-    /**
-     * Enqueue frontend scripte
-     */
-    public function ldmft_enqueue_front_scripts() {
-
-        /**
-         * Enqueue frontend css
-         */
-        wp_enqueue_style( 'dashicons' );
-        wp_enqueue_style( 'rcpl-jqueryui-css', 'https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css', [], RCPL_VERSION, null );
-        wp_enqueue_style( 'rcpl-front-css', RCPL_ASSETS_URL . 'css/frontend.css', [], RCPL_VERSION, null );
-        wp_enqueue_style( 'front-select-min-css', RCPL_ASSETS_URL .'css/select2.min.css' );
-        wp_enqueue_style( 'rcpl-custom-popup-css-link', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css', [], RCPL_VERSION, null );
-
-        /**
-         * Enqueue frontend js
-         */
-        wp_enqueue_script('rcpl-jqueryui-js', 'https://code.jquery.com/ui/1.10.4/jquery-ui.js', ['jquery'], RCPL_VERSION, true);
-        wp_enqueue_script( 'front-select2-jquery-js', RCPL_ASSETS_URL. 'js/select2.full.min.js', ['jquery'], RCPL_VERSION, true );
-        wp_enqueue_script( 'rcpl-select2-addition', RCPL_ASSETS_URL . 'js/backend-select2-addition.js', [ 'jquery' , 'front-select2-jquery-js' ], RCPL_VERSION, true ); 
-        wp_enqueue_script( 'wt-custom-bootstrap-pop-up-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js', ['jquery'], RCPL_VERSION, true );
-        
-        //
-        wp_enqueue_script( 'rcpl-backend-js', RCPL_ASSETS_URL . 'js/backend.js', [ 'jquery' , 'front-select2-jquery-js' ], RCPL_VERSION, true ); 
-        wp_enqueue_script( 'rcpl-backend-additional-js', RCPL_ASSETS_URL . 'js/backend-additional.js', [ 'jquery' ], RCPL_VERSION, true ); 
-        wp_enqueue_script( 'rcpl-frontend-js', RCPL_ASSETS_URL . 'js/frontend.js', [ 'jquery' ], RCPL_VERSION, true ); 
-        wp_enqueue_script( 'rcpl-schedule-js', RCPL_ASSETS_URL . 'js/rcpl-schedule.js', [ 'jquery' ], RCPL_VERSION, true ); 
-
-        wp_localize_script( 'rcpl-backend-js', 'RCPL', array( 
-            'ajaxURL' => admin_url( 'admin-ajax.php' ),
-        ) );
-
-        wp_localize_script( 'rcpl-backend-js', 'RcplNonce', array( 
-            'security' => wp_create_nonce( 'ldmft_ajax_nonce' )
-        ) );
     }
 
     /**
@@ -117,10 +80,10 @@ class ldFMT_Reviews_Shortcode {
         if( isset( $plugins->plugins ) &&  count($plugins->plugins) > 0 ) {
             $plugins = $plugins->plugins;
             $plugin = $plugins[0];
-
             ob_start();
-
             ?>
+                <link rel="stylesheet" href="<?php echo LDNFT_ASSETS_URL;?>lightbox/css/lightbox.min.css">
+                <script src="<?php echo LDNFT_ASSETS_URL;?>lightbox/js/lightbox-plus-jquery.min.js"></script>
                 <div class="ldmft_wrapper">
                     <div class="filter">
                         <label><?php echo __( 'Select a Plugin:', LDNFT_TEXT_DOMAIN );?></label>
@@ -129,9 +92,6 @@ class ldFMT_Reviews_Shortcode {
                                 foreach( $plugins as $plugin ) {
                                         
                                     $selected = '';
-                                    // if( $selected_plugin_id == $plugin->id ) {
-                                    //     $selected = ' selected = "selected"';   
-                                    // }
                                     ?>
                                         <option value="<?php echo $plugin->id; ?>" <?php echo $selected; ?>><?php echo $plugin->title; ?></option>
                                     <?php   
@@ -149,6 +109,7 @@ class ldFMT_Reviews_Shortcode {
                         </div> -->
                     </div>
                 </div>
+                
             <?php
             $content = ob_get_contents();
             ob_get_clean();
