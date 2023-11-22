e4e3c578e23526d5e398feec0ad0b45c2af59bb2
Muhammad Kamran <muhammadkamran.ldninjas@gmail.com>
2023-11-22 13:35:30 +0500

    Created separate content for review items

--- includes/shortcodes/templates/reviews/onetime.php
+++ includes/shortcodes/templates/reviews/onetime.php
@@ -5,5 +5,39 @@
 
 foreach( $results as $review ) {
 
-    echo LDNFT_Reviews_Shortcode::instance()->display_review_item( $review ); 
+    $client_name = isset( $review->name ) ? $review->name : '';
+    $rating = isset( $review->rate ) ? $review->rate : '';
+    $title = isset( $review->title ) ? $review->title : '';
+    $description = isset( $review->text ) ? $review->text : '';
+    $client_profile_pic = isset( $review->profile_url ) ? $review->profile_url : ''; 
+    ?>
+    <div class="slider-item ldnft-reviews-wrapper">
+        <div class="ldnft-reviews-profile-wrap">
+            <div class="ldnft-review-profile-img">
+                <img src="<?php echo !empty( $client_profile_pic ) ? $client_profile_pic : LDNFT_ASSETS_URL .'images/customer-profile.png';?>">
+            </div>
+            <div class="ldnft-review-client-name"><?php echo $client_name; ?></div>
+        </div>
+        <div class="ldnft-rating-wrapper">
+            <div class="ldnft-rating-div">
+              <?php 
+                  for( $i = 1; $i <= 5; $i++ ) {
+
+                      $selected = '';
+                      if( $i * 2 <= $rating ) {
+                          $selected = 'ldnft-checked';
+                      }
+                      echo '<span class="fa fa-star '.$selected.'"></span>';
+                  }
+              ?>
+            </div>
+        </div>
+        <div class="ldnft-reviews-title-wrapper">
+            <?php echo $title; ?>
+        </div>
+        <div class="ldnft-reviews-description-wrapper">
+            <?php echo $description; ?>
+        </div>
+    </div>
+    <?php 
 }
--- includes/shortcodes/templates/reviews/pagination.php
+++ includes/shortcodes/templates/reviews/pagination.php
@@ -8,7 +8,42 @@ $slide_ctrl     = '';
 $slide_index = 0;
 foreach( $results as $review ) {
 
-    echo LDNFT_Reviews_Shortcode::instance()->display_review_item( $review ); 
+    $client_name = isset( $review->name ) ? $review->name : '';
+    $rating = isset( $review->rate ) ? $review->rate : '';
+    $title = isset( $review->title ) ? $review->title : '';
+    $description = isset( $review->text ) ? $review->text : '';
+    $client_profile_pic = isset( $review->profile_url ) ? $review->profile_url : '';
+    ?>
+
+    <div class="slider-item ldnft-reviews-wrapper">
+        <div class="ldnft-reviews-profile-wrap">
+            <div class="ldnft-review-profile-img">
+                <img src="<?php echo !empty( $client_profile_pic ) ? $client_profile_pic : LDNFT_ASSETS_URL .'images/customer-profile.png'; ?>">
+            </div>
+            <div class="ldnft-review-client-name"><?php echo $client_name; ?></div>
+        </div>
+        <div class="ldnft-rating-wrapper">
+            <div class="ldnft-rating-div">
+              <?php 
+                  for( $i = 1; $i <= 5; $i++ ) {
+
+                      $selected = '';
+                      if( $i * 2 <= $rating ) {
+                          $selected = 'ldnft-checked';
+                      }
+                      echo '<span class="fa fa-star '.$selected.'"></span>';
+                  }
+              ?>
+            </div> 
+        </div>
+        <div class="ldnft-reviews-title-wrapper">
+            <?php echo $title; ?>
+        </div>
+        <div class="ldnft-reviews-description-wrapper">
+            <?php echo $description; ?>
+        </div>
+    </div>
+    <?php
 }
 
 $result_check = $wpdb->get_results( $wpdb->prepare( "SELECT r.*, c.email as useremail FROM $table_name where is_featured = 1 and r.plugin_id = %d ORDER BY r.id LIMIT %d OFFSET %d", $plugin_id, $per_page, ( $offset+$per_page ) ) );
